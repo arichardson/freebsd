c391fc9cc2831d8008299556fef73cfea54e492b
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-02-07 15:54:58 +0000

    crossbuild: build the libc db code during bootstrap on MacOS and Linux
    The Linux db functions create databases that are incompatible with what
    libc handles so the databases created during the install phase will not be
    readable unless we use the same db code to create them.
    To ensure that the bootstrap tools always link against this library instead
    of the system one I define dbopen() to __freebsd_dbopen() before including
    db.h so that we get linker errors instead of accidentally linking against
    the host dbopen().

---
+++ tools/build/cross-build/libdb_freebsd/namespace.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define _open(...) open(__VA_ARGS__)
+#define _close(a) close(a)
+#define _fstat(a, b) fstat(a, b)
+#define _read(a, b, c) read(a, b, c)
+#define _write(a, b, c) write(a, b, c)
+#define _writev(a, b, c) writev(a, b, c)
+#define _fsync(a) fsync(a)
