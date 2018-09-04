6bd9c87ed4285e2de54dcde666b90acb75c3449b
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-09-04 17:17:51 +0100

    Fix sys/endian.h warnings on Linux

--- tools/build/cross-build/include/linux/sys/endian.h
+++ tools/build/cross-build/include/linux/sys/endian.h
@@ -1,44 +1,24 @@
 #pragma once
 #include_next <sys/endian.h>
 
-#ifndef bswap64
+
+/* Ensure all these are constant expressions (which is not the case for some
+ * of the glibc versions depending on compiler optimization level) */
+
+#undef bswap64
 #define bswap64(a) __builtin_bswap64(a)
-#endif
 
-#ifndef bswap32
+#undef bswap32
 #define bswap32(a) __builtin_bswap32(a)
-#endif
 
-#ifndef bswap16
+#undef bswap16
 #define bswap16(a) __builtin_bswap16(a)
-#endif
 
-#if _BYTE_ORDER == _LITTLE_ENDIAN
-#define	htobe16(x)	bswap16((x))
-#define	htobe32(x)	bswap32((x))
-#define	htobe64(x)	bswap64((x))
-#define	htole16(x)	((uint16_t)(x))
-#define	htole32(x)	((uint32_t)(x))
-#define	htole64(x)	((uint64_t)(x))
+#undef __bswap_64
+#define __bswap_64(a) __builtin_bswap64(a)
 
-#define	be16toh(x)	bswap16((x))
-#define	be32toh(x)	bswap32((x))
-#define	be64toh(x)	bswap64((x))
-#define	le16toh(x)	((uint16_t)(x))
-#define	le32toh(x)	((uint32_t)(x))
-#define	le64toh(x)	((uint64_t)(x))
-#else /* _BYTE_ORDER != _LITTLE_ENDIAN */
-#define	htobe16(x)	((uint16_t)(x))
-#define	htobe32(x)	((uint32_t)(x))
-#define	htobe64(x)	((uint64_t)(x))
-#define	htole16(x)	bswap16((x))
-#define	htole32(x)	bswap32((x))
-#define	htole64(x)	bswap64((x))
+#undef __bswap_32
+#define __bswap_32(a) __builtin_bswap32(a)
 
-#define	be16toh(x)	((uint16_t)(x))
-#define	be32toh(x)	((uint32_t)(x))
-#define	be64toh(x)	((uint64_t)(x))
-#define	le16toh(x)	bswap16((x))
-#define	le32toh(x)	bswap32((x))
-#define	le64toh(x)	bswap64((x))
-#endif /* _BYTE_ORDER == _LITTLE_ENDIAN */
+#undef __bswap_16
+#define __bswap_16(a) __builtin_bswap16(a)
