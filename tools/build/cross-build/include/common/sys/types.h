9c12cd57d403e0c950581ca8b287353ae06ccd39
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-09-07 11:20:24 +0100

    Add capsicum helpers to -legacy
    This should allow us to bootstrap tools that use capsicum_helpers on older
    versions of FreeBSD

--- tools/build/cross-build/include/common/sys/types.h
+++ tools/build/cross-build/include/common/sys/types.h
@@ -14,3 +14,17 @@ typedef uintptr_t __uintptr_t;
 typedef int __nl_item;
 
 typedef size_t u_register_t;
+
+
+/* capsicum compat: */
+#ifndef _CAP_IOCTL_T_DECLARED
+#define	_CAP_IOCTL_T_DECLARED
+typedef	unsigned long	cap_ioctl_t;
+#endif
+
+#ifndef _CAP_RIGHTS_T_DECLARED
+#define	_CAP_RIGHTS_T_DECLARED
+struct cap_rights;
+
+typedef	struct cap_rights	cap_rights_t;
+#endif
