aea7d4db92b177b96e2c8275a9b5e3a4148b4f1f
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-12-14 20:03:39 +0000

    Reduce diff

--- tools/build/cross-build/include/linux/getopt.h
+++
@@ -1,9 +0,0 @@
-// Note: getopt.h is included multiple times by glibc so we can't use #pragma once here
-#include_next <getopt.h>
-
-__BEGIN_DECLS
-int __freebsd_getopt(int argc, char * const argv[], const char *shortopts);
-__END_DECLS
-
-#undef getopt
-#define getopt(argc, argv, optstr) __freebsd_getopt(argc, argv, optstr)
