a48867079d93c454d3eb0030485b28d690715e1a
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-08-11 15:40:47 +0100

    Fix case insensitive (D16762)

--- share/mk/bsd.links.mk
+++ share/mk/bsd.links.mk
@@ -13,10 +13,27 @@ TAG_ARGS=      -T ${TAGS:[*]:S/ /,/g}
 
 afterinstall: _installlinks
 .ORDER: realinstall _installlinks
+# If the source and destination file are the same file (e.g. Mail/mail on a
+# case-insensitive file-system) we have to pass -S to install(1) to avoid
+# deleting the source file.
 _installlinks:
 .for s t in ${LINKS}
+.if ${s:tl} == ${t:tl}
+	if test "${DESTDIR}${t}" -ef "${DESTDIR}${s}"; then \
+		echo "Note: requested hard link from ${t} to ${s} on case-insensitive file system."; \
+	fi
+	${INSTALL_LINK} -S ${TAG_ARGS} ${DESTDIR}${s} ${DESTDIR}${t}
+.else
 	${INSTALL_LINK} ${TAG_ARGS} ${DESTDIR}${s} ${DESTDIR}${t}
+.endif
 .endfor
 .for s t in ${SYMLINKS}
+.if ${s:tl} == ${t:tl}
+	if test "${DESTDIR}${t}" -ef "${DESTDIR}${s}"; then \
+		echo "Note: requested symlink from ${t} to ${s} on case-insensitive file system."; \
+	fi
+	${INSTALL_SYMLINK} -S ${TAG_ARGS} ${s} ${DESTDIR}${t}
+.else
 	${INSTALL_SYMLINK} ${TAG_ARGS} ${s} ${DESTDIR}${t}
+.endif
 .endfor
