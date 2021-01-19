685b3a0ae2043cbea7841c916f0f391693ccbb04
Alex Richardson <arichardson@FreeBSD.org>
2021-01-19 11:35:10 +0000

    WIP: Wundef in contrib code

--- contrib/flex/src/flexdef.h
+++ contrib/flex/src/flexdef.h
@@ -827,7 +827,7 @@ extern void flexerror(const char *);
 extern void flexfatal(const char *);
 
 /* Report a fatal error with a pinpoint, and terminate */
-#if HAVE_DECL___FUNC__
+#ifdef __CC_SUPPORTS___FUNC__
 #define flex_die(msg) \
     do{ \
         fprintf (stderr,\
